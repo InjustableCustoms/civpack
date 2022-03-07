d73a565dc97672af57678a2fc9849474f7de1239
InjustableCustoms <skylineethany@gmail.com>
2022-03-07 17:38:15 +1030

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,393 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+
+--hsvmaloo 
+files {
+    'data/hsvmaloo/vehicles.meta',
+    'data/hsvmaloo/carvariations.meta',
+    'data/hsvmaloo/carcols.meta',
+    'data/hsvmaloo/handling.meta',
+    'data/hsvmaloo/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/hsvmaloo/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/hsvmaloo/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/hsvmaloo/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/hsvmaloo/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/hsvmaloo/vehiclelayouts.meta'
+
+
+--c63coupe
+files {
+    'data/c63coupe/vehicles.meta',
+    'data/c63coupe/carvariations.meta',
+    'data/c63coupe/carcols.meta',
+    'data/c63coupe/handling.meta',
+    'data/c63coupe/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/c63coupe/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/c63coupe/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/c63coupe/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/c63coupe/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/c63coupe/vehiclelayouts.meta'
+
+--gtrnismo17
+files {
+    'data/gtrnismo17/vehicles.meta',
+    'data/gtrnismo17/carvariations.meta',
+    'data/gtrnismo17/carcols.meta',
+    'data/gtrnismo17/handling.meta',
+    'data/gtrnismo17/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/gtrnismo17/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/gtrnismo17/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/gtrnismo17/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/gtrnismo17/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/gtrnismo17/vehiclelayouts.meta'
+
+--cfmotoz10
+files {
+    'data/cfmotoz10/vehicles.meta',
+    'data/cfmotoz10/carvariations.meta',
+    'data/cfmotoz10/carcols.meta',
+    'data/cfmotoz10/handling.meta',
+    'data/cfmotoz10/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/cfmotoz10/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/cfmotoz10/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/cfmotoz10/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/cfmotoz10/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/cfmotoz10/vehiclelayouts.meta'
+
+
+
+--amarok
+files {
+    'data/amarok/vehicles.meta',
+    'data/amarok/carvariations.meta',
+    'data/amarok/carcols.meta',
+    'data/amarok/handling.meta',
+    'data/amarok/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/amarok/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/amarok/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/amarok/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/amarok/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/amarok/vehiclelayouts.meta'
+
+--nis13
+files {
+    'data/nis13/vehicles.meta',
+    'data/nis13/carvariations.meta',
+    'data/nis13/carcols.meta',
+    'data/nis13/handling.meta',
+    'data/nis13/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/nis13/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/nis13/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/nis13/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/nis13/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/nis13/vehiclelayouts.meta'
+
+--gt86
+files {
+    'data/gt86/vehicles.meta',
+    'data/gt86/carvariations.meta',
+    'data/gt86/carcols.meta',
+    'data/gt86/handling.meta',
+    'data/gt86/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/gt86/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/gt86/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/gt86/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/gt86/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/gt86/vehiclelayouts.meta'
+
+--fj40
+files {
+    'data/fj40/vehicles.meta',
+    'data/fj40/carvariations.meta',
+    'data/fj40/carcols.meta',
+    'data/fj40/handling.meta',
+    'data/fj40/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/fj40/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/fj40/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/fj40/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/fj40/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/fj40/vehiclelayouts.meta'
+
+--evo9
+files {
+    'data/evo9/vehicles.meta',
+    'data/evo9/carvariations.meta',
+    'data/evo9/carcols.meta',
+    'data/evo9/handling.meta',
+    'data/evo9/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/evo9/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/evo9/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/evo9/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/evo9/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/evo9/vehiclelayouts.meta'
+
+
+--s15
+files {
+    'data/s15/vehicles.meta',
+    'data/s15/carvariations.meta',
+    'data/s15/carcols.meta',
+    'data/s15/handling.meta',
+    'data/s15/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/s15/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/s15/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/s15/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/s15/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/s15/vehiclelayouts.meta'
+
+--r32
+files {
+    'data/r32/vehicles.meta',
+    'data/r32/carvariations.meta',
+    'data/r32/carcols.meta',
+    'data/r32/handling.meta',
+    'data/r32/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/r32/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/r32/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/r32/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/r32/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/r32/vehiclelayouts.meta'
+
+--agt12
+files {
+    'data/agt12/vehicles.meta',
+    'data/agt12/carvariations.meta',
+    'data/agt12/carcols.meta',
+    'data/agt12/handling.meta',
+    'data/agt12/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/agt12/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/agt12/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/agt12/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/agt12/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/agt12/vehiclelayouts.meta'
+
+--widebodyled
+files {
+    'data/widebodyled/vehicles.meta',
+    'data/widebodyled/carvariations.meta',
+    'data/widebodyled/carcols.meta',
+    'data/widebodyled/handling.meta',
+    'data/widebodyled/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/widebodyled/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/widebodyled/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/widebodyled/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/widebodyled/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/widebodyled/vehiclelayouts.meta'
+
+--tesroad20
+files {
+    'data/tesroad20/vehicles.meta',
+    'data/tesroad20/carvariations.meta',
+    'data/tesroad20/carcols.meta',
+    'data/tesroad20/handling.meta',
+    'data/tesroad20/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/tesroad20/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/tesroad20/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/tesroad20/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/tesroad20/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/tesroad20/vehiclelayouts.meta'
+
+--huayrar
+files {
+    'data/huayrar/vehicles.meta',
+    'data/huayrar/carvariations.meta',
+    'data/huayrar/carcols.meta',
+    'data/huayrar/handling.meta',
+    'data/huayrar/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/huayrar/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/huayrar/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/huayrar/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/huayrar/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/huayrar/vehiclelayouts.meta'
+
+--amgone
+files {
+    'data/amgone/vehicles.meta',
+    'data/amgone/carvariations.meta',
+    'data/amgone/carcols.meta',
+    'data/amgone/handling.meta',
+    'data/amgone/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/amgone/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/amgone/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/amgone/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/amgone/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/amgone/vehiclelayouts.meta'
+
+--stringexp
+files {
+    'data/stringexp/vehicles.meta',
+    'data/stringexp/carvariations.meta',
+    'data/stringexp/carcols.meta',
+    'data/stringexp/handling.meta',
+    'data/stringexp/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/stringexp/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/stringexp/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/stringexp/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/stringexp/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/stringexp/vehiclelayouts.meta'
+
+--mfc
+files {
+    'data/mfc/vehicles.meta',
+    'data/mfc/carvariations.meta',
+    'data/mfc/carcols.meta',
+    'data/mfc/handling.meta',
+    'data/mfc/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/mfc/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/mfc/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/mfc/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/mfc/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/mfc/vehiclelayouts.meta'
+
+
+--warthog
+files {
+    'data/warthog/vehicles.meta',
+    'data/warthog/carvariations.meta',
+    'data/warthog/carcols.meta',
+    'data/warthog/handling.meta',
+    'data/warthog/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/warthog/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/warthog/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/warthog/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/warthog/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/warthog/vehiclelayouts.meta'
+
+--f150
+files {
+    'data/f150/vehicles.meta',
+    'data/f150/carvariations.meta',
+    'data/f150/carcols.meta',
+    'data/f150/handling.meta',
+    'data/f150/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/f150/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/f150/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/f150/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/f150/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/f150/vehiclelayouts.meta'
+
+--a90pit
+files {
+    'data/a90pit/vehicles.meta',
+    'data/a90pit/carvariations.meta',
+    'data/a90pit/carcols.meta',
+    'data/a90pit/handling.meta',
+    'data/a90pit/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/a90pit/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/a90pit/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/a90pit/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/a90pit/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/a90pit/vehiclelayouts.meta'
+
+--amggtrr20
+files {
+    'data/amggtrr20/vehicles.meta',
+    'data/amggtrr20/carvariations.meta',
+    'data/amggtrr20/carcols.meta',
+    'data/amggtrr20/handling.meta',
+    'data/amggtrr20/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/amggtrr20/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/amggtrr20/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/amggtrr20/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/amggtrr20/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/amggtrr20/vehiclelayouts.meta'
+
+--amggtsprior
+files {
+    'data/amggtsprior/vehicles.meta',
+    'data/amggtsprior/carvariations.meta',
+    'data/amggtsprior/carcols.meta',
+    'data/amggtsprior/handling.meta',
+    'data/amggtsprior/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/amggtsprior/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/amggtsprior/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/amggtsprior/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/amggtsprior/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/amggtsprior/vehiclelayouts.meta'
+
+--rmodlp770
+files {
+    'data/rmodlp770/vehicles.meta',
+    'data/rmodlp770/carvariations.meta',
+    'data/rmodlp770/carcols.meta',
+    'data/rmodlp770/handling.meta',
+    'data/rmodlp770/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/rmodlp770/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/rmodlp770/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/rmodlp770/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/rmodlp770/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/rmodlp770/vehiclelayouts.meta'
+
+--a45amg
+files {
+    'data/a45amg/vehicles.meta',
+    'data/a45amg/carvariations.meta',
+    'data/a45amg/carcols.meta',
+    'data/a45amg/handling.meta',
+    'data/a45amg/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/a45amg/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/a45amg/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/a45amg/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/a45amg/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/a45amg/vehiclelayouts.meta'   
+
+
+
+
+
+
+
+
+
+
+
